a18d9ff7a0cd6fb120b8ba4cfa43e344cb909ec5
Karmanya Singh <63588045+karma1505@users.noreply.github.com>
2025-10-04 23:28:45 +0530

    updated versions

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@
 
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: 'http://192.168.31.235:8080/api/v1',
+  BASE_URL: 'http://192.168.31.70:8080/api/v1',
   TIMEOUT: 30000, // 30 seconds
   RETRY_ATTEMPTS: 3,
 };
